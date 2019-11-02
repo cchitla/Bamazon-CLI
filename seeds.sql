18f4e6ebfe7761ae447174b25c33da5b7c98ecb4
Chris Chitla <chrischitla@gmail.com>
2019-11-01 20:27:04 -0700

    initial files, mysql schema and seed files

---
+++ seeds.sql
@@ -0,0 +1,31 @@
+USE bamazon_db;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("18V Drill Set", "Tools", "199.99", "7");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Router", "Tools", "74.99", "5");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Chop Saw", "Tools", "249.99", "3");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dave Smith Prophet 6 Synthesizer", "Musical Instruments", "2799.99", "2");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Les Paul Electric Guitar", "Musical Instruments", "999.99", "4");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Drum Kit", "Musical Instruments", "499.99", "5");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPad Pro", "Electronics", "799.99", "10");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Surface Laptop 3", "ELectronics", "999.99", "8");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('50" 4K TV', "Electronics", "399.99", "9");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("A6000 Mirrorless Camera", "Electronics", "599.99", "6");
